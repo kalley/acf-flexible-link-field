1df81fcf59f395805f9c79deb9b1e6ae31eb3684
Kalley <kalley.powell@gmail.com>
2013-08-29 14:07:39 -0500

    If using as just a freeform URL, can't make it readonly

--- flexible_link-v4.php
+++ flexible_link-v4.php
@@ -220,6 +220,21 @@ class acf_field_flexible_link extends acf_field_page_link
 
 	}
 
+  /*
+	*  format_value_for_api()
+	*
+	*  This filter is appied to the $value after it is loaded from the db and before it is passed back to the api functions such as the_field
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value	- the value which was loaded from the database
+	*  @param	$post_id - the $post_id from which the value was loaded
+	*  @param	$field	- the field array holding all the field options
+	*
+	*  @return	$value	- the modified value
+	*/
 
 	function format_value_for_api( $value, $post_id, $field )
 	{
@@ -229,6 +244,32 @@ class acf_field_flexible_link extends acf_field_page_link
 		}
 
 		return $value;
+	}
+
+
+	/*
+	*  update_field()
+	*
+	*  This filter is appied to the $field before it is saved to the database
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$field - the field array holding all the field options
+	*  @param	$post_id - the field group ID (post_type = acf)
+	*
+	*  @return	$field - the modified field
+	*/
+
+	function update_field($field, $post_id)
+	{
+		if ( ! $field['use_pages'] )
+		{
+  		$field['freeform'] = true;
+		}
+
+		return $field;
 	}
 
 }
