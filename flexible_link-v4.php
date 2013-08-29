14b513daf987fe9e1aaa4026cf71f3500d4d86d7
Kalley <kalley.powell@gmail.com>
2013-08-29 14:03:05 -0500

    Added ability to use it as just a freeform URL field

--- flexible_link-v4.php
+++ flexible_link-v4.php
@@ -27,6 +27,7 @@ class acf_field_flexible_link extends acf_field_page_link
 			'multiple' => 0,
 			'allow_null' => 1,
 			'freeform' => 1,
+			'use_pages' => 1,
 		);
 
 
@@ -61,6 +62,27 @@ class acf_field_flexible_link extends acf_field_page_link
 	  $key = $field['name'];
 
 		?>
+<tr class="field_option field_option_<?php echo $this->name; ?> field_option_<?php echo $this->name; ?>_toggle">
+	<td class="label">
+		<label for=""><?php _e("Select from Pages?",'acf-flexible_link'); ?></label>
+	</td>
+	<td>
+		<?php
+
+		do_action('acf/create_field', array(
+			'type'	=>	'radio',
+			'name'	=>	'fields['.$key.'][use_pages]',
+			'value'	=>	$field['use_pages'],
+			'choices'	=>	array(
+				1	=>	__("Yes",'acf'),
+				0	=>	__("No",'acf'),
+			),
+			'layout'	=>	'horizontal',
+		));
+
+		?>
+	</td>
+</tr>
 <tr class="field_option field_option_<?php echo $this->name; ?>">
 	<td class="label">
 		<label for=""><?php _e("Post Type",'acf'); ?></label>
@@ -126,14 +148,41 @@ class acf_field_flexible_link extends acf_field_page_link
 	function create_field( $field )
 	{
 	  echo '<label for="' . $field['id'] . '">Enter a URL</label>';
-	  echo '<input type="text" id="' . $field['id'] . '" class="' . $field['class'] . '" name="' . $field['name'] . '" value="' . $field['value'] . '"><br>';
-	  echo '<p class="howto toggle-arrow" id="internal-toggle" style="background-image:url(/wp-includes/images/toggle-arrow.png);padding-left:18px;">Or link to existing content</p>';
+	  echo '<input type="text" id="' . $field['id'] . '" class="' . $field['class'] . '" name="' . $field['name'] . '" value="' . $field['value'] . '"' . ( $field['freeform'] ? '' : ' readonly' ) . '>';
 
-	  $field['id'] = '';
-	  $field['name'] = '';
-	  $field['class'] .= '-select';
+	  if ( $field['use_pages'] )
+	  {
+  	  echo '<p class="howto toggle-arrow" id="internal-toggle" style="background-image:url(/wp-includes/images/toggle-arrow.png);padding-left:18px;">Or link to existing content</p>';
+  	  $field['id'] = '';
+  	  $field['name'] = '';
+  	  $field['class'] .= '-select';
 
-		parent::create_field($field);
+  		parent::create_field($field);
+	  }
+
+	}
+
+
+	/*
+	*  field_group_admin_enqueue_scripts()
+	*
+	*  This action is called in the admin_enqueue_scripts action on the edit screen where your field is edited.
+	*  Use this action to add css + javascript to assist your create_field_options() action.
+	*
+	*  $info	http://codex.wordpress.org/Plugin_API/Action_Reference/admin_enqueue_scripts
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function field_group_admin_enqueue_scripts()
+	{
+		wp_register_script( 'acf-field-group-flexible_link', $this->settings['dir'] . 'js/field-group.js', array('acf-field-group'), $this->settings['version']);
+
+		// scripts
+		wp_enqueue_script(array(
+			'acf-field-group-flexible_link',
+		));
 	}
 
 
