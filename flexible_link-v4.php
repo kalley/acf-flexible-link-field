b1e155b97130518b6cc984d5ab7ce19d3d9253ef
Kalley <kalley.powell@gmail.com>
2013-08-29 12:18:35 -0500

    Fixed output of url

--- flexible_link-v4.php
+++ flexible_link-v4.php
@@ -129,7 +129,7 @@ class acf_field_flexible_link extends acf_field_page_link
 	  echo '<input type="text" id="' . $field['id'] . '" class="' . $field['class'] . '" name="' . $field['name'] . '" value="' . $field['value'] . '"><br>';
 	  echo '<p class="howto toggle-arrow" id="internal-toggle" style="background-image:url(/wp-includes/images/toggle-arrow.png);padding-left:18px;">Or link to existing content</p>';
 
-	  $field['id'] .= '-select';
+	  $field['id'] = '';
 	  $field['name'] = '';
 	  $field['class'] .= '-select';
 
@@ -172,135 +172,15 @@ class acf_field_flexible_link extends acf_field_page_link
 	}
 
 
-	/*
-	*  load_value()
-	*
-	*  This filter is appied to the $value after it is loaded from the db
-	*
-	*  @type	filter
-	*  @since	3.6
-	*  @date	23/01/13
-	*
-	*  @param	$value - the value found in the database
-	*  @param	$post_id - the $post_id from which the value was loaded from
-	*  @param	$field - the field array holding all the field options
-	*
-	*  @return	$value - the value to be saved in te database
-	* /
-
-	function load_value($value, $post_id, $field)
+	function format_value_for_api( $value, $post_id, $field )
 	{
-		// Note: This function can be removed if not used
+		if( !$value OR $value == 'null' )
+		{
+			return false;
+		}
+
 		return $value;
 	}
-
-
-	/*
-	*  update_value()
-	*
-	*  This filter is appied to the $value before it is updated in the db
-	*
-	*  @type	filter
-	*  @since	3.6
-	*  @date	23/01/13
-	*
-	*  @param	$value - the value which will be saved in the database
-	*  @param	$post_id - the $post_id of which the value will be saved
-	*  @param	$field - the field array holding all the field options
-	*
-	*  @return	$value - the modified value
-	* /
-
-	function update_value($value, $post_id, $field)
-	{
-		// Note: This function can be removed if not used
-		return $value;
-	}
-
-
-	/*
-	*  format_value()
-	*
-	*  This filter is appied to the $value after it is loaded from the db and before it is passed to the create_field action
-	*
-	*  @type	filter
-	*  @since	3.6
-	*  @date	23/01/13
-	*
-	*  @param	$value	- the value which was loaded from the database
-	*  @param	$post_id - the $post_id from which the value was loaded
-	*  @param	$field	- the field array holding all the field options
-	*
-	*  @return	$value	- the modified value
-	* /
-
-	function format_value($value, $post_id, $field)
-	{
-		// defaults?
-		/*
-		$field = array_merge($this->defaults, $field);
-		* /
-
-		// perhaps use $field['preview_size'] to alter the $value?
-
-
-		// Note: This function can be removed if not used
-		return $value;
-	}
-
-
-	/*
-	*  format_value_for_api()
-	*
-	*  This filter is appied to the $value after it is loaded from the db and before it is passed back to the api functions such as the_field
-	*
-	*  @type	filter
-	*  @since	3.6
-	*  @date	23/01/13
-	*
-	*  @param	$value	- the value which was loaded from the database
-	*  @param	$post_id - the $post_id from which the value was loaded
-	*  @param	$field	- the field array holding all the field options
-	*
-	*  @return	$value	- the modified value
-	* /
-
-	function format_value_for_api($value, $post_id, $field)
-	{
-		// defaults?
-		/*
-		$field = array_merge($this->defaults, $field);
-		* /
-
-		// perhaps use $field['preview_size'] to alter the $value?
-
-
-		// Note: This function can be removed if not used
-		return $value;
-	}
-
-
-	/*
-	*  update_field()
-	*
-	*  This filter is appied to the $field before it is saved to the database
-	*
-	*  @type	filter
-	*  @since	3.6
-	*  @date	23/01/13
-	*
-	*  @param	$field - the field array holding all the field options
-	*  @param	$post_id - the field group ID (post_type = acf)
-	*
-	*  @return	$field - the modified field
-	* /
-
-	function update_field($field, $post_id)
-	{
-		// Note: This function can be removed if not used
-		return $field;
-	}
-  /* */
 
 }
 
